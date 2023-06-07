2648037c335d3cb82b9fed88ce72306fc0ff4411
Patidifusa <zs20006765@estudiantes.uv.mx>
2023-06-07 18:20:41 -0500

    inicialize #1

---
+++ src/main/java/org/uv/examen1_romeroreyeslilliam/Alumno.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.uv.examen1_romeroreyeslilliam;
+
+/**
+ *
+ * @author LilyE
+ */
+
+public class Alumno {
+    
+}
