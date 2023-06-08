1e65b9270fee8e42f3808df95755434a970affa8
Patidifusa <zs20006765@estudiantes.uv.mx>
2023-06-07 19:10:18 -0500

    update #2

--- src/main/java/org/uv/examen1_romeroreyeslilliam/Alumno.java
+++ src/main/java/org/uv/examen1_romeroreyeslilliam/Alumno.java
@@ -4,11 +4,61 @@
  */
 package org.uv.examen1_romeroreyeslilliam;
 
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 /**
  *
  * @author LilyE
  */
-
+@Entity
+@Table(name = "alumno")
 public class Alumno {
     
+    @Id
+    @Column(name = "clave")
+    private long clave;
+    
+    @Column(name = "nombre")
+    private String nombre;
+    
+    @Column(name = "direccion")
+    private String direccion;
+    
+    @Column(name = "telefono")
+    private String telefono;
+
+    public long getClave() {
+        return clave;
+    }
+
+    public void setClave(long clave) {
+        this.clave = clave;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
 }
